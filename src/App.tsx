b28af2008a8110f9f5630d543f26ab219d22ec9e
Francis Boily <boilyfrank28@gmail.com>
2017-10-29 11:20:59 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>TEST HELLO WORLD</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
