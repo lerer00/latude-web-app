b046a99abe4f19f9f0b9f0849f9cd4c6cef907b1
Francis Boily <boilyfrank28@gmail.com>
2017-12-11 21:48:13 -0500

    Front page polish

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import * as React from 'react';
 import { Jumbotron } from './jumbotron';
-// import { Tile } from './tile';
 import { Button } from './button';
 import './App.css';
 
-// const egoSquirrel = require('./img/ego/squirrel.svg');
-// const egoApple = require('./img/ego/apple.svg');
-// const egoLeaf = require('./img/ego/leaf.svg');
-// const egoShield = require('./img/ego/shield.svg');
-// const egoWallet = require('./img/ego/wallet.svg');
-// const egoClock = require('./img/ego/clock.svg');
-// const egoGramophone2 = require('./img/ego/gramophone-2.svg');
-// const egoTram = require('./img/ego/tram.svg');
-// const egoBusStop = require('./img/ego/bus-stop.svg');
-// const egoMultiPlatform = require('./img/ego/multi-platform.svg');
-// const egoPizza = require('./img/eg/pizza.svg');
 const egoBusinessWorkStation2 = require('./img/ego/business-work-station-2.svg');
 const egoDevices = require('./img/ego/devices.svg');
 const egoLab = require('./img/ego/lab.svg');
@@ -23,6 +11,7 @@ const egoLogout1 = require('./img/ego/logout-1.svg');
 const egoMultiPlatform2 = require('./img/ego/multi-platform-2.svg');
 const egoNetwork = require('./img/ego/network.svg');
 const egoTransferFile = require('./img/ego/transfer-file.svg');
+const egoStandingDesk = require('./img/ego/standing-desk.svg');
 
 class App extends React.Component {
   constructor() {
@@ -89,7 +78,7 @@ class App extends React.Component {
               <div className='container'>
                 <div className='row'>
                   <div className='col-2' />
-                  <div className='col-8 col'>
+                  <div className='col-8'>
                     <h1 className='title on-light line-separated'>Read our whitepaper</h1>
                     <p className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     <Button
@@ -108,7 +97,7 @@ class App extends React.Component {
               <div className='container'>
                 <div className='row'>
                   <div className='col-2' />
-                  <div className='col-8 col'>
+                  <div className='col-8'>
                     <h1 className='title on-dark line-separated'>latude. network</h1>
                     <p className='description on-dark'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                   </div>
@@ -158,21 +147,54 @@ class App extends React.Component {
               <div className='container timeline'>
                 <div className='row'>
                   <div className='col-2' />
-                  <div className='col-8 col'>
+                  <div className='col-8'>
                     <h1 className='title on-light line-separated'>Timeline</h1>
                     <p className='description on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                   </div>
                   <div className='col-2' />
                 </div>
                 <div className='row'>
-                  <div className='col-3 line-division'>
+                  <div className='col-3 line-division line-division-after'>
                     <div className='line-event'>
                       <img className='icon' src={egoMultiPlatform2} />
                     </div>
                   </div>
-                  <div className='col-9 col'>
+                  <div className='col-9'>
                     <h1 className='title-2 on-light'>January 2018</h1>
-                    <p className='description description-left on-light'>Bootstrap that project</p>
+                    <p className='description description-left on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-3 line-division line-division-after'>
+                    <div className='line-event'>
+                      <img className='icon' src={egoMultiPlatform2} />
+                    </div>
+                  </div>
+                  <div className='col-9'>
+                    <h1 className='title-2 on-light'>June 2017</h1>
+                    <p className='description description-left on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-3 line-division line-division-after'>
+                    <div className='line-event'>
+                      <img className='icon' src={egoMultiPlatform2} />
+                    </div>
+                  </div>
+                  <div className='col-9'>
+                    <h1 className='title-2 on-light'>February 2018</h1>
+                    <p className='description description-left on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-3 line-division line-division-after'>
+                    <div className='line-event'>
+                      <img className='icon' src={egoMultiPlatform2} />
+                    </div>
+                  </div>
+                  <div className='col-9'>
+                    <h1 className='title-2 on-light'>May 2018</h1>
+                    <p className='description description-left on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                   </div>
                 </div>
                 <div className='row'>
@@ -181,17 +203,37 @@ class App extends React.Component {
                       <img className='icon' src={egoMultiPlatform2} />
                     </div>
                   </div>
-                  <div className='col-9 col'>
-                    <h1 className='title-2 on-light'>January 2018</h1>
-                    <p className='description description-left on-light'>Bootstrap that project</p>
+                  <div className='col-9'>
+                    <h1 className='title-2 on-light'>July 2018</h1>
+                    <p className='description description-left on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='footer white-division'>
+          <div className='dashed-division'>
             <div className='centered-division'>
-              <p className='on-light'>All rights reserved. latude. inc.</p>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-6'>
+                    <h1 className='title on-light line-separated'>Wanna help</h1>
+                    <p className='description on-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                    <Button
+                      text='Get in touch'
+                      icon={egoLogout1}
+                      action={this.whitepaperAction}
+                    />
+                  </div>
+                  <div className='col-6'>
+                    <img src={egoStandingDesk} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='footer blue-division'>
+            <div className='centered-division'>
+              <p className='on-dark'>All rights reserved. latude. inc.</p>
             </div>
           </div>
         </div>
